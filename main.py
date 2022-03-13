696651a04411adf82a2ee9dcb15fff30b45a3d5f
Mario Castro <mariocastro.pva@gmail.com>
2022-03-13 15:52:04 -0400

    Templates y manejo de errores

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-from flask import Flask, request, make_response, redirect
+from multiprocessing import context
+from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)
+todo_list = ["Comprar cafe", "Dormir", "Comer"]
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template("404.html", error=error)
+
+
+@app.errorhandler(500)
+def server_error(error):
+    return render_template("500.html", error=error)
 
 
 @app.route("/")
@@ -14,7 +26,9 @@ def index():
 @app.route("/hello")
 def hello():
     user_ip = request.cookies.get("user_ip")
-    return f"Hola Mario desde {user_ip}"
+    context = {"user_ip": user_ip, "todo_list": todo_list}
+
+    return render_template("hello.html", **context)
 
 
 if __name__ == "__main__":
