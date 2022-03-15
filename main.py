9843712a7cb2747dc6f9152405e136f2e6d4477b
Mario Castro <mariocastro.pva@gmail.com>
2022-03-15 11:07:13 -0400

    Flask-WTF & Tests

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from imghdr import tests
 import os
 from dotenv import load_dotenv
 from flask import (
@@ -5,13 +6,21 @@ from flask import (
     request,
     make_response,
     redirect,
+    url_for,
     render_template,
     session,
+    flash,
 )
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+from flask_bootstrap import Bootstrap
+import unittest
 
 load_dotenv()
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 app.config["SECRET_KEY"] = os.getenv("FLASK_SECRET")
 app.config.update(
@@ -21,6 +30,18 @@ app.config.update(
 )
 
 todo_list = ["Comprar cafe", "Dormir", "Comer"]
+
+
+class LoginForm(FlaskForm):
+    username = StringField("Nombre de Usuario", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Enviar")
+
+
+@app.cli.command()
+def test():
+    tests = unittest.TestLoader().discover("tests")
+    unittest.TextTestRunner().run(tests)
 
 
 @app.errorhandler(404)
@@ -41,10 +62,25 @@ def index():
     return response
 
 
-@app.route("/hello")
+@app.route("/hello", methods=["GET", "POST"])
 def hello():
     user_ip = session.get("user_ip")
-    context = {"user_ip": user_ip, "todo_list": todo_list}
+    login_form = LoginForm()
+    username = session.get("username")
+
+    context = {
+        "user_ip": user_ip,
+        "todo_list": todo_list,
+        "login_form": login_form,
+        "username": username,
+    }
+
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        session["username"] = username
+        flash("Nombre de usuario registrado con exito")
+
+        return redirect(url_for("index"))
 
     return render_template("hello.html", **context)
 
